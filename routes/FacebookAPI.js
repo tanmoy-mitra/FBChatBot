b2b8947b7e0d767ca7791eda4b9dd3143c50f0dd
Tanmoy Mitra <tanmoy@imaginei.in>
2016-12-06 17:16:22 +0530

    FBChatBot First Commit

---
+++ routes/FacebookAPI.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+});
+
+module.exports = router;
