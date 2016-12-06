e3b35d1a8868cce22b0706a8d548ea4c7ca74c26
Tanmoy Mitra <tanmoy@imaginei.in>
2016-12-06 17:50:50 +0530

    No.4

--- routes/FacebookAPI.js
+++ routes/FacebookAPI.js
@@ -3,10 +3,32 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if (req.query['hub.verify_token'] === 'tanmoy8447696675mitra') {
-        res.send(req.query['hub.challenge']);
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
     }
-    res.send('Error, wrong token');
+    res.sendStatus(200);
 });
+
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
 
 module.exports = router;
